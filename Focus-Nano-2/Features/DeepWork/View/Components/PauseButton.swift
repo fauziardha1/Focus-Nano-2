c162d672e84a299fd8f4b490ff5e27a72bacee04
fauziardha1 <fauziardasaputra@gmail.com>
2022-07-28 13:26:08 +0700

    add alert to pause button

--- Focus-Nano-2/Features/DeepWork/View/Components/PauseButton.swift
+++ Focus-Nano-2/Features/DeepWork/View/Components/PauseButton.swift
@@ -10,9 +10,13 @@ import SwiftUI
 
 struct PauseButton: View {
     let g : GeometryProxy
+    @Environment(\.dismiss) var dismiss
+    @State var isPauseAlertShows = false
     
     var body: some View {
-        Button(action:{}) {
+        Button(action:{
+            isPauseAlertShows.toggle()
+        }) {
             HStack{
                 Text("Pause")
                     .foregroundColor(.white)
@@ -23,6 +27,22 @@ struct PauseButton: View {
             .cornerRadius(22)
             .padding(.horizontal)
             
+        }
+        .alert(isPresented: $isPauseAlertShows ) {
+                    Alert(
+                        title: Text("OH NO!"),
+                        message: Text("Are you sure to pause this session?"),
+                        primaryButton:  .default(
+                            Text("No, back to session")
+                                .foregroundColor(Color(UIColor.green.darker()!))
+                        ),
+                        secondaryButton: .destructive(
+                            Text("Yes, Pause"),
+                            action: {dismiss()}
+                        )
+                        
+                        
+                    )
         }
     }
 }
